15403bb7776cf022a16b79ca674a7751fb3205a2
Falentino <falentinodjoka2801@gmail.com>
2023-01-09 16:43:55 +0700

    Belajar Python 9 Januari 2023

---
+++ 2023/9Januari2023/is-it-bug-or-feature.py
@@ -0,0 +1,13 @@
+myList  =   [
+    "one", "two", "three" "four", "five"
+]
+print(len(myList)) #4
+
+#It returns 4. I've not figured it out how it can be happened. Is it bug or a feature in python, IDK.
+
+name    =   "Falentino" "Djoka"
+print(name)
+
+#Now I know the answer
+#If you put two or more adjacent strings, it means your strings will be concatenated. For example, variable name above
+#consist of two strings "Falentino", and "Djoka". Python automatically concat these string, and the result is "FalentinoDjoka"
